f8c028b252861bab12b5f246dd550c05a98e73f2
Ken <99477459+zengjunlin2022@users.noreply.github.com>
2024-01-08 18:13:48 +0800

    feat: +CatchError type

--- js/app.js
+++ js/app.js
@@ -4,8 +4,9 @@ const App = new Vue({
     // 類型
     type: "待消費緩存",
     types: [
-      { text: "待消費緩存", value: "0" },
-      { text: "設備全部緩存", value: "1" },
+      { text: "待消費緩存", value: "0", sort: true },
+      { text: "設備全部緩存", value: "1", sort: true },
+      { text: "catchError", value: "2", sort: false },
     ],
     // 輸入
     content: "",
@@ -16,6 +17,7 @@ const App = new Vue({
     submit() {
       let that = this;
       if (that.content === "") return;
+      let typeIdx = that.types.findIndex((t) => t.text == that.type);
       let list = JSON.parse(that.content).data.items;
       let result = [];
       list.forEach((item) => {
@@ -26,21 +28,30 @@ const App = new Vue({
           msg = that.parseWaitPostCount(JSON.parse(msg));
         } else if (that.type == "設備全部緩存") {
           msg = that.parseAllPostCount(JSON.parse(msg));
+        } else if (that.type == "catchError") {
+          msg = that.parseCatchError(JSON.parse(msg));
         }
-        let idx = result.findIndex((r) => r.userName == msg.userName);
-        if (idx == -1) {
-          result.push(msg);
-        } else {
-          if (msg.data > result[idx].data) {
-            result[idx] = msg;
+
+        if (that.types[typeIdx].sort) {
+          let idx = result.findIndex((r) => r.userName == msg.userName);
+          if (idx == -1) {
+            result.push(msg);
+          } else {
+            if (msg.data > result[idx].data) {
+              result[idx] = msg;
+            }
           }
+        } else {
+          result.push(msg);
         }
       });
 
       // 倒序排序
-      result.sort((a, b) => {
-        return b.data - a.data;
-      });
+      if (that.types[typeIdx].sort) {
+        result.sort((a, b) => {
+          return b.data - a.data;
+        });
+      }
       that.result = result
         .map((item) => {
           return `${item.userName}：${item.data}`;
@@ -57,6 +68,16 @@ const App = new Vue({
       };
     },
     parseAllPostCount(msg) {
+      // {"userName":"T2606","funcName":"store-actions","apiName":"Schedule_Queue_V2_length","data":6,"time":"2024-01-08 17:57:37","device":"Redmi-Xiaomi 22041211AC"}
+      let userName = msg.userName;
+      let data = msg.data;
+      return {
+        userName,
+        data,
+      };
+    },
+    parseCatchError(msg) {
+      console.log(msg);
       // {"userName":"T2606","funcName":"store-actions","apiName":"Schedule_Queue_V2_length","data":6,"time":"2024-01-08 17:57:37","device":"Redmi-Xiaomi 22041211AC"}
       let userName = msg.userName;
       let data = msg.data;
