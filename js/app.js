01f7ae2b06050766d9b98047e069bea260703b7c
Ken <99477459+zengjunlin2022@users.noreply.github.com>
2024-01-10 10:20:01 +0800

    feat: 調整輸出結果

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ const App = new Vue({
     // 類型
     type: "待消費緩存",
     types: [
-      { text: "待消費緩存", value: "0", sort: true },
-      { text: "設備全部緩存", value: "1", sort: true },
-      { text: "catchError", value: "2", sort: false },
+      { text: "待消費緩存", value: "0", wraningThreshold: 3 },
+      { text: "設備全部緩存", value: "1", wraningThreshold: 12 },
+      { text: "catchError", value: "2", wraningThreshold: null },
     ],
     // 輸入
     content: "",
@@ -17,9 +17,8 @@ const App = new Vue({
     submit() {
       let that = this;
       if (that.content === "") return;
-      let typeIdx = that.types.findIndex((t) => t.text == that.type);
       let list = JSON.parse(that.content).data.items;
-      let result = [];
+      let result = {};
       list.forEach((item) => {
         // 轉碼
         let msg = decodeURIComponent(item.msg);
@@ -32,48 +31,68 @@ const App = new Vue({
           msg = that.parseCatchError(JSON.parse(msg));
         }
 
-        if (that.types[typeIdx].sort) {
-          let idx = result.findIndex((r) => r.userName == msg.userName);
-          if (idx == -1) {
-            result.push(msg);
-          } else {
-            if (msg.data > result[idx].data) {
-              result[idx] = msg;
-            }
-          }
-        } else {
-          result.push(msg);
+        // 按工號聚合
+        if (!result.hasOwnProperty(msg.userName)) {
+          result[msg.userName] = [];
         }
+        result[msg.userName].push(msg);
+        // 按時間排序
+        result[msg.userName].sort((a, b) => {
+          return dayjs(b.time).valueOf() - dayjs(a.time).valueOf();
+        });
       });
 
-      // 倒序排序
-      if (that.types[typeIdx].sort) {
-        result.sort((a, b) => {
-          return b.data - a.data;
+      that.result = that.obj2str(result);
+    },
+    getWraningClass(type, data) {
+      let that = this;
+      let typeIdx = that.types.findIndex((t) => t.text == type);
+      let wraningThreshold = that.types[typeIdx].wraningThreshold;
+      if (wraningThreshold == null) return "";
+      if (data > wraningThreshold) return "wraning";
+    },
+    obj2str(result) {
+      let that = this;
+      let str = "";
+      for (let staffId in result) {
+        str += `<p><strong>${staffId}</strong></p>`;
+        result[staffId].forEach((item) => {
+          str += `
+          <p>
+            <span class="${that.getWraningClass(that.type, item.data)}">
+              ${item.data} ${item.time}
+            </span>
+            <span>${item.device}</span>
+          </p>`;
         });
+        str += "</br></br>";
       }
-      that.result = result
-        .map((item) => {
-          return `${item.userName}：${item.data}`;
-        })
-        .join("\n");
+      return str;
     },
     parseWaitPostCount(msg) {
       // {"userName":"T2899","funcName":"updateWaitPostCacheCount","apiName":"waitPostCacheCount","data":{"data":0},"time":"2024-01-08 16:44:50","device":"iPhone-iPhone14,2"}
       let userName = msg.userName;
       let data = msg.data.data;
+      let time = msg.time;
+      let device = msg.device;
       return {
         userName,
         data,
+        time,
+        device,
       };
     },
     parseAllPostCount(msg) {
       // {"userName":"T2606","funcName":"store-actions","apiName":"Schedule_Queue_V2_length","data":6,"time":"2024-01-08 17:57:37","device":"Redmi-Xiaomi 22041211AC"}
       let userName = msg.userName;
       let data = msg.data;
+      let time = msg.time;
+      let device = msg.device;
       return {
         userName,
         data,
+        time,
+        device,
       };
     },
     parseCatchError(msg) {
@@ -81,9 +100,13 @@ const App = new Vue({
       // {"userName":"T2606","funcName":"store-actions","apiName":"Schedule_Queue_V2_length","data":6,"time":"2024-01-08 17:57:37","device":"Redmi-Xiaomi 22041211AC"}
       let userName = msg.userName;
       let data = msg.data;
+      let time = msg.time;
+      let device = msg.device;
       return {
         userName,
         data,
+        time,
+        device,
       };
     },
   },
